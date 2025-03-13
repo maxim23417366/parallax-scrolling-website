c26ca670a6a81d2b5f36d001b3e5decacb758c9a
Maksym Neporozhnii <max.n.200206@gmail.com>
2025-03-13 23:17:37 +0200

    commit 2

--- ready-html/js/app.js
+++ ready-html/js/app.js
@@ -11,3 +11,37 @@ ScrollSmoother.create({
   smooth: false
 });
 
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.utils.toArray("article").forEach(article => {
+    gsap.to(article, {
+        backgroundPosition: "50% 100%", // змінює позицію фону під час скролу
+        ease: "none",
+        scrollTrigger: {
+            trigger: article,
+            start: "top bottom",
+            end: "bottom top",
+            scrub: true // плавний ефект під час прокручування
+        }
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  let observerOptions = {
+      root: null, // Відстежуємо відносно вікна браузера
+      threshold: 0.1 // 10% елемента має бути видно
+  };
+
+  let observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              entry.target.classList.add("visible");
+          } else {
+              entry.target.classList.remove("visible");
+          }
+      });
+  }, observerOptions);
+
+  document.querySelectorAll(".second-article__header, .second-article__paragraph, .third-article__header, .third-article__paragraph")
+      .forEach(element => observer.observe(element));
+});
